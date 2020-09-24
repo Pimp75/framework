7798a2f42f4606847b8b9ccba28b8b436549c110
Bastien LACOMBE <bastien.lacombe@manomano.com>
2020-09-24 09:30:25 +0200

    Change default value for hello route

--- src/app.php
+++ src/app.php
@@ -5,7 +5,7 @@ use Symfony\Component\Routing\Route;
 
 $routes = new RouteCollection();
 
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'hello']));
+$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
 $routes->add('bye', new Route('/bye'));
 
 return $routes;
