15b4cb92668881c388e0df0569cfcd1d0be35c9c
Bastien LACOMBE <bastien.lacombe@manomano.com>
2020-09-28 13:16:59 +0200

    Move code to HttpKernel

--- public/index.php
+++ public/index.php
@@ -3,39 +3,15 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 use Symfony\Component\HttpKernel\HttpCache\Store;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 use Pimp\Framework;
-use Calendar\StringResponseListener;
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 
 $routes = include __DIR__.'/../src/app.php';
 
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new ErrorListener('Calendar\Controller\ErrorController::exception'));
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new StringResponseListener());
-
-
-$framework = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+$framework = new Framework($routes);
 
 $framework = new HttpCache($framework, new Store(__DIR__.'../var/cache'));
 
